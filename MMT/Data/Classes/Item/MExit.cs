33fb1673ac1adaad64a6c025621c48373b0d7c9d
2816484844@qq.com <2816484844@qq.com>
2021-05-25 21:31:16 +0800

    出口

---
+++ MMT/Data/Classes/Item/MExit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMT.Data.Classes.Item
+{
+    class MExit
+    {
+        private bool exit;//bool=1为出口，bool=0为上一关入口
+        public bool Exit { get => exit; set => exit = value; }
+        //构造函数
+        public MExit(bool exit)
+        {
+            Exit = exit;
+        }
+    }
+}
