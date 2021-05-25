6d87e805789135f0f4320ba43b227d94280251a1
Eimondy <823624595@qq.com>
2021-05-25 21:43:02 +0800

    逻辑

--- MMT/MMainLogic.cs
+++ MMT/MMainLogic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using MMT.Data.Classes;
@@ -58,7 +59,7 @@ namespace MMT
 
         public void GameOver()
         {
-
+            
         }
 
         public void Draw()
@@ -74,7 +75,6 @@ namespace MMT
         public void Exit()
         {
             MMainForm.Instance.Dispose();
-            //MMainForm.Instance.Close();
         }
 
         public void NewGame(string pn)
@@ -106,17 +106,59 @@ namespace MMT
             Saves.Add(CurrentProfile);
         }
 
-        public void CombatMode()
+        public void CombatMode(MCharacter enemy)     // 类型后续需改为MEnemy
         {
+            Combat = true;
+            bool fightOver = false;
+            // 比较速度判出先后手
+            MCharacter currentOne = null;
+            // 将主角的HP、MP、Power提到Max值
+            while (!fightOver)
+            {
+                if(currentOne is MMainCharacter)     // 主角的轮次。若30秒之内不攻击，则使用普通攻击。
+                {
+                    int i = 0;
+                    while (i < 30)               // 后续需更改。等待MMAinCharacter的新增属性AttackChoice : int
+                    {
+                        Thread.Sleep(1000);
+                        i++;
+                    }
+                    // 根据AttackChoice来进行攻击操作
 
+                    // 判断对方血量、是否胜利
+                }
+                else     // 敌人的轮次
+                {
+                    // 随机选择技能进行攻击操作
+
+                    // 判断对方血量、是否胜利
+                }
+            }
+            Combat = false;
+            // 若主角战败，则调用DefeatedMode()
+
+            // 若敌人为关底Boss，则调用VictoryMode()
         }
 
         public void VictoryMode()
         {
-
+            Victory = true;
+            // 调用MMainForm.Instance.EndingMenu()
+            // 展示统计信息
         }
 
         public void DefeatedMode()
+        {
+            Defeated = true;
+            // 调用MMainForm.Instance.EndingMenu()
+        }
+
+        public void StarFromCurrentLevel()     // 战斗失败之后，从当前关卡重玩
+        {
+
+        }
+
+        public void BackToMainMenu()     // 游戏胜利之后/战斗失败之后，返回主菜单
         {
 
         }
