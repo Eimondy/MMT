7bd8e971b60686f2de2e266ec39174c152040e35
Eimondy <823624595@qq.com>
2021-05-26 12:12:24 +0800

    出口类。增加实现Interact的方法体，增加复制构造函数

--- MMT/Data/Classes/Item/MExit.cs
+++ MMT/Data/Classes/Item/MExit.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,24 @@ using System.Threading.Tasks;
 
 namespace MMT.Data.Classes.Item
 {
-    class MExit
+    public class MExit : MItem
     {
         private bool exit;//bool=1为出口，bool=0为上一关入口
         public bool Exit { get => exit; set => exit = value; }
         //构造函数
-        public MExit(bool exit)
+        public MExit(string name, Bitmap image, byte locationX, byte locationY,bool exit=false):base(name,image,locationX,locationY)
         {
             Exit = exit;
+        }
+
+        public MExit(MExit exit) : base(exit.Name, exit.Image, exit.LocationX, exit.LocationY)
+        {
+            Exit = exit.Exit;
+        }
+
+        public override void Interact()
+        {
+            throw new NotImplementedException();
         }
     }
 }
