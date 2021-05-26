9ff257bc091b4b806f51f92a1727de2253d038f7
Eimondy <823624595@qq.com>
2021-05-26 10:46:57 +0800

    敌人类，MONSTER枚举已定义，增加默认构造函数声明

--- MMT/Data/Classes/Character/MEnemy.cs
+++ MMT/Data/Classes/Character/MEnemy.cs
@@ -4,21 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace MMT.Data.Classes.Character
 {
-    enum MONSTER {
-        ORDINARY,
-        ELITE,
-        BOSS 
-    }
-    public class MEnemy : MCharacter
+    public abstract class MEnemy : MCharacter
     {
-        internal MONSTER monstertype; //敌人类型
-        public string description; //描述
+        private MONSTER monstertype; //敌人类型
+        private string description; //描述
 
         public string Description { get { return Description; }set { description = value; } }
-        internal MONSTER MonsterType { get { return MonsterType; }set { monstertype = value; } }
-    }
+        public MONSTER MonsterType { get { return MonsterType; }set { monstertype = value; } }
+        
+        public MEnemy()
+        {
 
+        }
+    }
 }
