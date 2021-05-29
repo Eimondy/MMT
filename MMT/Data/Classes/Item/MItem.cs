18ebe6f764b2cd111ebf3ed590d85ba4d8e71d9f
Eimondy <823624595@qq.com>
2021-05-29 14:49:25 +0800

    物品

--- MMT/Data/Classes/Item/MItem.cs
+++ MMT/Data/Classes/Item/MItem.cs
@@ -19,7 +19,11 @@ namespace MMT.Data.Classes.Item
         public byte LocationX { get => locationX; set => locationX = value; }
         public byte LocationY { get => locationY; set => locationY = value; }
         //道具交互方法
-        public abstract void Interact();
+        public virtual void Interact()
+        {
+            // 将该物品记录至GameProfile当中
+            MMainLogic.Instance.CurrentProfile.ItemCount.Add(this);
+        }
         //构造方法
         public MItem(string name, Bitmap image, byte locationX, byte locationY)
         {
@@ -35,5 +39,10 @@ namespace MMT.Data.Classes.Item
             LocationX = item.LocationX;
             LocationY = item.LocationY;
         }
+        /*
+        public override bool Equals(object obj)     // 用于从Item列表中移除某个对象时，判断两个Item对象是否相等。因为一个关卡中，每个Item的位置都不同，所以只需要判断名称和位置是否相等即可
+        {
+            return (obj as MItem).Name == Name && (obj as MItem).LocationX == LocationX && (obj as MItem).LocationY == LocationY;
+        }*/
     }
 }
