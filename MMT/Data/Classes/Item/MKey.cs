93a45fe9f445e5ce3e906a74904bce8c9cbcdd7a
2816484844@qq.com <2816484844@qq.com>
2021-05-29 16:52:51 +0800

    钥匙类，完善构造方法

--- MMT/Data/Classes/Item/MKey.cs
+++ MMT/Data/Classes/Item/MKey.cs
@@ -8,19 +8,19 @@ using MMT.Data.Classes.Character;
 
 namespace MMT.Data.Classes.Item
 {
-    class MKey :MItem, MIPickable
+    class MKey : MItem, MIPickable
     {
+
         private byte relatedDoor;//和钥匙对应的门
         public byte RelatedDoor { get => relatedDoor; set => relatedDoor = value; }
         //构造函数
-        public MKey(string name, Bitmap image, byte locationX, byte locationY,byte relatedDoor):base(name,image,locationX,locationY)
+        public MKey(byte locationX, byte locationY, byte relatedDoor) : base(locationX, locationY)
         {
+            Name = "钥匙";
+            //Image=new Bitmap();
             RelatedDoor = relatedDoor;
         }
-        public MKey(MKey key) : base(key.Name, key.Image, key.LocationX, key.LocationY)
-        {
-           RelatedDoor = key.RelatedDoor;
-        }
+
         public override void Interact()
         {
             // 执行父类的交互操作，将钥匙记录到GameProfile当中
@@ -33,7 +33,7 @@ namespace MMT.Data.Classes.Item
             // 将该钥匙放入主角的钥匙栏中
             MMainCharacter.Instance.Keys.Add(this);
             // 将该钥匙从关卡中移除
-            MLevel.Levels[MLevel.CurrentLevel].Items.Remove(this);
+            MLevel.Levels[MLevel.CurrentLevel - 1].Items.Remove(this);
         }
     }
 }
