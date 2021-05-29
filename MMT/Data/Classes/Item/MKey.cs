be038f9304800b5317ab97aad52c4e3a1d523b30
Eimondy <823624595@qq.com>
2021-05-29 14:49:34 +0800

    钥匙

--- MMT/Data/Classes/Item/MKey.cs
+++ MMT/Data/Classes/Item/MKey.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MMT.Data.Classes.Character;
 
 namespace MMT.Data.Classes.Item
 {
-    public class MKey :MItem
+    class MKey :MItem, MIPickable
     {
         private byte relatedDoor;//和钥匙对应的门
         public byte RelatedDoor { get => relatedDoor; set => relatedDoor = value; }
@@ -22,7 +23,17 @@ namespace MMT.Data.Classes.Item
         }
         public override void Interact()
         {
-            throw new NotImplementedException();
+            // 执行父类的交互操作，将钥匙记录到GameProfile当中
+            base.Interact();
+            Picked();
+        }
+
+        public void Picked()
+        {
+            // 将该钥匙放入主角的钥匙栏中
+            MMainCharacter.Instance.Keys.Add(this);
+            // 将该钥匙从关卡中移除
+            MLevel.Levels[MLevel.CurrentLevel].Items.Remove(this);
         }
     }
 }
