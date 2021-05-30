cbf2890634d1b976e8e80a9681a818df77663740
Eimondy <823624595@qq.com>
2021-05-30 21:06:50 +0800

    主角技能。添加引用，将字段引用改为属性引用

--- MMT/Data/Classes/Skill/Beat.cs
+++ MMT/Data/Classes/Skill/Beat.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+using MMT.Data.Classes.Character;
 
 namespace MMT.Data.Classes.Skill
 {
 
     //角色技能
     //beat
-    class Beat : MSkill
+    public class Beat : MSkill
     {
         public Beat()
         {
-            name = "Beat"; //技能名称
-            points = 1.2f;//伤害倍数
-            consumption = 20;//所消耗的对应属性的值
-            type = ATTRIBUTE.POWER;//技能类型,仅POWER和MAGIC
-            description = "普通b级物理技能，伤害倍数1.2";//技能描述
+            Name = "Beat"; //技能名称
+            Points = 1.2f;//伤害倍数
+            Consumption = 20;//所消耗的对应属性的值
+            Type = ATTRIBUTE.POWER;//技能类型,仅POWER和MAGIC
+            Description = "普通b级物理技能，伤害倍数1.2";//技能描述
         }
 
 
@@ -21,7 +22,7 @@ namespace MMT.Data.Classes.Skill
         {
 
             //若角色体力不足，返回
-            if (MMainCharacter.Instance.Power < consumption)
+            if (MMainCharacter.Instance.Power < Consumption)
             {
                 // Console.WriteLine("体力不够");
                 return;
@@ -32,7 +33,7 @@ namespace MMT.Data.Classes.Skill
             var Attack = 0.0;
             if (p < MMainCharacter.Instance.HitRate) //命中
             {
-                Attack = MMainCharacter.Instance.Power * points * 2.4;
+                Attack = MMainCharacter.Instance.Power * Points * 2.4;
             }
             else //未命中
             {
