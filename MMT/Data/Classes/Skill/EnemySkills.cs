d6d2380cea2598f220b2d32c19b9c38ed7747e57
wangvv222@icloud.com <wangvv222@icloud.com>
2021-06-04 18:47:10 +0800

    技能类基本完成

--- MMT/Data/Classes/Skill/EnemySkills.cs
+++ MMT/Data/Classes/Skill/EnemySkills.cs
@@ -22,9 +22,9 @@ namespace MMT.Data.Classes.Skill
             double p = rd.NextDouble();
             if (p < user.HitRate) //命中
             {
-                var Attack = enemy.Power * Points * COMBAT.ATTACK;
-                var TakeAttack = Attack - user.Armor * COMBAT.DEFENSE;
-                user.HP = user.HP - (int)TakeAttack; //这里把伤害转成整型了
+                var Attack = user.MaxPower * Points * COMBAT.ATTACK;
+                var TakeAttack = Attack - enemy.Armor * COMBAT.DEFENSE;
+                enemy.HP = enemy.HP - (int)TakeAttack; //这里把伤害转成整型了
             }
             return true;
         }
@@ -56,4 +56,226 @@ namespace MMT.Data.Classes.Skill
             return true;
         }
     }
+
+    public class EEvilFluctuation : MSkill
+    {
+        public EEvilFluctuation()
+        {
+            Name = "EvilFluctuation";
+            Points = 1.2f;
+            Consumption = 30;
+            Type = ATTRIBUTE.POWER;
+            Description = "物理技能，命中后使主角进入疲惫状态，虚弱主角20%力量";
+        }
+        public override bool Activate(MCharacter user, MCharacter enemy)
+        {
+            if (!base.Activate(user, enemy)) return false;
+            Random rd = new Random();
+            double p = rd.NextDouble();
+            if (p < user.HitRate)
+            {
+                enemy.Power -= Convert.ToInt32(enemy.Power * 0.2);
+            }
+            return true;
+        }
+    }
+    public class EDeadWinding:MSkill
+    { 
+        public EDeadWinding()
+        {
+            Name = "DeadWinding";
+            Points = 1.2;
+            Consumption = 40;
+            Type = ATTRIBUTE.MAGIC;
+            Description = "法术技能，伤害倍数1.2";
+        }
+        public override bool Activate(MCharacter user, MCharacter enemy)
+        {
+            if (!base.Activate(user, enemy)) return false;
+            Random rd = new Random();
+            double p = rd.NextDouble();
+            if (p < user.HitRate)
+            {
+                var Attack = user.MaxMP * Points * COMBAT.ATTACK;
+                var TakeAttack = Attack - enemy.MagicArmor * COMBAT.DEFENSE;
+                enemy.HP -= (int)TakeAttack; //这里把伤害转成整型了
+            }
+            return true;
+        }
+    }
+
+    //3个Boss的技能全都用B开头命名
+
+    public class BDevilChopper:MSkill
+    {
+        public BDevilChopper()
+        {
+            Name = "DevilChopper";
+            Points = 1.2;
+            Consumption = 80;
+            Type = ATTRIBUTE.POWER;
+            Description = "骷髅将军专属物理技能，命中后有50%概率进行二次攻击，第二次攻击伤害倍数减为1.1";
+
+        }
+        public override bool Activate(MCharacter user, MCharacter enemy)
+        {
+            if (!base.Activate(user, enemy)) return false;
+            Random rd = new Random();
+            double p = rd.NextDouble();
+            if (p > user.HitRate) return true; 
+            //第一次攻击
+            var Attack = user.MaxPower * Points * COMBAT.ATTACK;
+            var TakeAttack = Attack - enemy.Armor * COMBAT.DEFENSE;
+            enemy.HP -= (int)TakeAttack; //这里把伤害转成整型了
+            // 1/2的概率进行第二次攻击
+            if (p <= 0.5) return true;
+            //第二次攻击,新生成一个随机数
+            Random rd = new Random();
+            double p2 = rd.NextDouble();
+            if (p2 > user.HitRate) return true;
+            var SecondAttack = user.MaxPower  * Points * COMBAT.ATTACK;
+            var SecondTakeAttack = SecondAttack - enemy.Armor * COMBAT.DEFENSE;
+            enemy.HP -= (int)SecondTakeAttack; //这里把伤害转成整型了
+            return true;
+
+        }
+    }
+    public class BSarifice:MSkill
+    {
+        public BSarifice()
+        {
+            Name = "Sarifice";
+            Points = 1.3;
+            Consumption = 50;
+            Type = ATTRIBUTE.MAGIC;
+            Description = "骷髅将军专属法术技能，伤害倍数1.3";
+
+        }
+        public override bool Activate(MCharacter user, MCharacter enemy)
+        {
+            if (!base.Activate(user, enemy)) return false;
+            Random rd = new Random();
+            double p = rd.NextDouble();
+            if (p < user.HitRate)
+            {
+                var Attack = user.MaxMP * Points * COMBAT.ATTACK;
+                var TakeAttack = Attack - enemy.MagicArmor * COMBAT.DEFENSE;
+                enemy.HP -= (int)TakeAttack; //这里把伤害转成整型了
+            }
+            return true;
+        }
+    }
+
+    public class BSoulKnife : MSkill
+    {
+        public BSoulKnife()
+        {
+            Name = "SoulKnife";
+            Points = 1.2;
+            Consumption = 50;
+            Type = ATTRIBUTE.POWER;
+            Description = "大法师专属物理技能，伤害倍数1.2且降低主角40%魔抗";
+        }
+        public override bool Activate(MCharacter user, MCharacter enemy)
+        {
+            if (!base.Activate(user, enemy)) return false;
+            Random rd = new Random();
+            double p = rd.NextDouble();
+            if (p < user.HitRate)
+            {
+                var Attack = user.MaxPower * Points * COMBAT.ATTACK;
+                var TakeAttack = Attack - enemy.Armor * COMBAT.DEFENSE;
+                enemy.MP -= Convert.ToInt32(enemy.MP * 0.4);
+                enemy.HP -= (int)TakeAttack; //这里把伤害转成整型了
+            }
+            return true;
+
+        }
+    }
+    public class BSoulGift : MSkill 
+    {
+        public BSoulGift()
+        {
+            Name = "SoulGift";
+            Points = 1.4;
+            Consumption = 125;
+            Type = ATTRIBUTE.MAGIC;
+            Description = "大法师专属法术技能，伤害倍数1.4，且提高自身100%护甲值";
+        }
+        public override bool Activate(MCharacter user, MCharacter enemy)
+        {
+            if (!base.Activate(user, enemy)) return false;
+            Random rd = new Random();
+            double p = rd.NextDouble();
+            if (p < user.HitRate)
+            {
+                var Attack = user.MaxMP * Points * COMBAT.ATTACK;
+                var TakeAttack = Attack - enemy.MagicArmor * COMBAT.DEFENSE;
+                user.Armor *= 2;
+                enemy.HP -= (int)TakeAttack; //这里把伤害转成整型了
+            }
+            return true;
+
+        }
+    }
+    public class BSoulChop : MSkill
+    {
+        public BSoulChop()
+        {
+            Name = "SoulChop";
+            Points = 1.5;
+            Consumption = 200;
+            Type = ATTRIBUTE.POWER;
+            Description = "魔王专属物理技能，伤害倍数2，同时清楚自身一切负面效果，无视对方一切技能加成";
+
+        }
+        public override bool Activate(MCharacter user, MCharacter enemy)
+        {
+            if (!base.Activate(user, enemy)) return false;
+            Random rd = new Random();
+            double p = rd.NextDouble();
+            if (p < user.HitRate)
+            {
+                var Attack = user.MaxPower * Points * COMBAT.ATTACK;
+                var TakeAttack = Attack - enemy.Armor * COMBAT.DEFENSE;
+                enemy.HP -= (int)TakeAttack; //这里把伤害转成整型了
+
+                //无视一切技能加成还未完成
+            }
+            return true;
+        }
+    }
+    public class BDarkCurse : MSkill
+    {
+        public BDarkCurse()
+        {
+            Name = "DarkCurse";
+            Points = 1.5;
+            Consumption = 200;
+            Type = ATTRIBUTE.MAGIC;
+            Description = "魔王专属法术技能，伤害倍数2，30%概率打出二次攻击，二次攻击伤害倍数减为1.3";
+
+        }
+
+        public override bool Activate(MCharacter user, MCharacter enemy)
+        {
+            if (!base.Activate(user, enemy)) return false;
+            Random rd = new Random();
+            double p = rd.NextDouble();
+            if (p > user.HitRate) return true;
+            var Attack = user.MaxMP * Points * COMBAT.ATTACK;
+            var TakeAttack = Attack - enemy.MagicArmor * COMBAT.DEFENSE;
+            enemy.HP -= (int)TakeAttack; //这里把伤害转成整型了
+
+            double p2 = rd.NextDouble();
+            if (p2 > 0.33) return true;
+            //第二次攻击points变为1.3
+            var SecondAttack = user.MaxMP * 1.3 * COMBAT.ATTACK;
+            var SecondTakeAttack = Attack - enemy.MagicArmor * COMBAT.DEFENSE;
+            enemy.HP -= (int)SecondTakeAttack; //这里把伤害转成整型了
+            return true;
+        }
+    }
+
+
 }
