ee180eef2bc9ee2bda3e12c1c5e5492fec8d613e
Eimondy <823624595@qq.com>
2021-06-13 16:23:57 +0800

    资源

--- MMT/Properties/Resources.Designer.cs
+++ MMT/Properties/Resources.Designer.cs
@@ -733,6 +733,16 @@ namespace MMT.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap Img_pillar2 {
+            get {
+                object obj = ResourceManager.GetObject("Img_pillar2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap Img_scroll {
             get {
                 object obj = ResourceManager.GetObject("Img_scroll", resourceCulture);
